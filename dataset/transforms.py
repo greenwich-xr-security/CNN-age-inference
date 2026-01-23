17fb7d0e76e2dc5fbd4bf0ca3098421b4a537a76
Riccardo Bovo <rb1619@ic.ac.uk>
2026-01-23 14:06:56 +0000

    Update transforms.py

--- dataset/transforms.py
+++ dataset/transforms.py
@@ -8,12 +8,12 @@ def build_transforms(img_size: int):
     """Return train/test transforms for a given square image size."""
     train_transform = transforms.Compose(
         [
-            #transforms.Resize((img_size, img_size)),
-            transforms.RandomResizedCrop(img_size, scale=(0.7, 1.0)),
-            transforms.RandomRotation(degrees=(-180, 180)),
+            transforms.Resize((img_size, img_size)),
+            # transforms.RandomResizedCrop(img_size, scale=(0.7, 1.0)),
+            transforms.RandomRotation(degrees=(-20, 20)),
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),
-            transforms.ColorJitter(brightness=0.2, contrast=0.2),
+            transforms.ColorJitter(brightness=0.01, contrast=0.01),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
